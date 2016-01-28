5393277f52a6b20ac564bd127ae1b800daad35db
Anthony Ou <azou@ualberta.ca>
2016-01-27 22:44:02 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 
+## A simple crate client with pooled http fallbacks based on the poolee library.
 
-to run
+
+### To run
 
 ```javascript
 var crateClient = require('crate-client')
 var client = new crateClient('localhost:4200')
 client = new crateClient(['localhost:4200', 'localhost:4400'])
+
+client.exec('select * from users where ? = id', [42])
 ```
--- index.js
+++ index.js
@@ -1,43 +1,40 @@
-var Pool, crateClient, http, util;
+var Pool,
+    crateClient,
+    http,
+    util;
 
 http = require("http");
-
 Pool = require("poolee");
-
 util = require('util');
 
-crateClient = (function() {
-  function crateClient(endpoints, options) {
-    if (options == null) {
-      options = {};
-    }
-    if (util.isString(endpoints)) {
-      this.pool = new Pool(http, [endpoints], options);
-    } else if (util.isArray(endpoints)) {
-      this.pool = new Pool(http, [endpoints], options);
-    }
+crateClient = function crateClient(endpoints, options) {
+  if (options == null) {
+    options = {};
   }
+  if (util.isString(endpoints)) {
+    this.pool = new Pool(http, [endpoints], options);
+  } else if (util.isArray(endpoints)) {
+    this.pool = new Pool(http, [endpoints], options);
+  }
+}
 
-  crateClient.prototype.exec = function(statment, args, cb) {
-    return this.pool.request({
-      method: 'POST',
-      path: '/_sql'
-    }, {
-      stmt: statment,
-      args: args
-    }, function(err, response, body) {
-      if (!util.isfunction(cb)) {
-        return;
-      }
-      if (err || response !== 200) {
-        return cb(err, response, body);
-      }
-      return cb(null, body);
-    });
-  };
-
-  return crateClient;
-
-})();
+crateClient.prototype.exec = function(statment, args, cb) {
+  this.pool.request({
+    method: 'POST',
+    path: '/_sql'
+  }, {
+    stmt: statment,
+    args: args
+  }, function(err, response, body) {
+    if (!util.isfunction(cb)) {
+      return;
+    }
+    if (err || response !== 200) {
+      return cb(err, response, body);
+    }
+    return cb(null, body);
+  });
+  return this;
+};
 
 module.exports = crateClient;
